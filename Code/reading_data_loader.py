063f6074ce1374a0b14eb3bdccfc5a999bb585e0
ianjali <amudgal26@gwmail.gwu.edu>
2023-12-09 02:50:02 -0500

    pushing updated splits

--- Code/reading_data_loader.py
+++ Code/reading_data_loader.py
@@ -4,6 +4,8 @@ import os
 import pickle
 import numpy as np
 from torch.utils.data import Dataset, DataLoader
+from transformers import BertTokenizer
+import pandas as pd
 '''
 to run this file :
 
@@ -35,43 +37,38 @@ final_test = 'final_test.csv'
 FINAL_TRAIN_FILE = DATA_DIR + final_train
 FINAL_TEST_FILE = DATA_DIR + final_test
 
+
+tokenizer_ = BertTokenizer.from_pretrained('bert-base-uncased')
+MAX_LENGTH = 128
 ##model files
-LABEL_ENCODER_FILE = MODEL_DIR + 'label_encoder.csv'
+#LABEL_ENCODER_FILE = MODEL_DIR + 'label_encoder.csv'
 TRAIN_DATA_LOADER =  MODEL_DIR + 'train_loader.pkl'
 VAL_DATA_LOADER =  MODEL_DIR + 'val_loader.pkl'
-
+TEST_DATA_LOADER =  MODEL_DIR + 'test_loader.pkl'
 
 input_column = 'tweet'
 output_column = ['class_encoded']
 NUM_CLASSES = 10
+
+BATCH_SIZE = 32
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("Using ", device)
-
-
 class CustomDataset(Dataset):
-    def __init__(self, dataframe, datatype,mapping_df, tokenizer_type =None, max_length = None, tokenizer = None):
+    def __init__(self, dataframe,  max_length = MAX_LENGTH, tokenizer = tokenizer_):
         self.dataframe = dataframe
-        self.tokenizer_type = tokenizer_type
         self.tokenizer = tokenizer
         self.max_length = max_length
-        self.datatype = datatype
-        self.mapping_df = mapping_df
     def __len__(self):
         return len(self.dataframe)
     def __getitem__(self, idx):
         input_text = self.dataframe.loc[idx, input_column]
-        if self.datatype == 'train':
-            label_cols = output_column
-            labels = self.dataframe.loc[idx, label_cols].values[0]
-            #label_encoded = self.mapping_df.transform(labels.values)
-            one_hot = np.eye(NUM_CLASSES)[labels].astype(int)
-            #label_list = [int(label) for label in encoded_new_data]
-            label_tensor = torch.tensor(one_hot, dtype=torch.float32)
-        if self.tokenizer_type == None:
-            if self.datatype == 'train':
-                return input_text, label_tensor
-            else:
-                return input_text
+        label_cols = output_column
+        labels = self.dataframe.loc[idx, label_cols].values[0]
+        one_hot = np.eye(NUM_CLASSES)[labels].astype(int)
+        label_tensor = torch.tensor(one_hot, dtype=torch.float32)
+        if self.tokenizer == None:
+            return input_text, label_tensor
         else:
             encoding = self.tokenizer.encode_plus(
                 input_text,
@@ -84,41 +81,43 @@ class CustomDataset(Dataset):
             )
             input_ids = encoding['input_ids'].squeeze(0)
             attention_mask = encoding['attention_mask'].squeeze(0)
-
-            if self.datatype == 'train':
-                return input_ids, attention_mask, label_tensor
-            else :
-                return input_ids, attention_mask
-
+            return input_ids, attention_mask, label_tensor
 class CustomDataLoader:
-    def __init__(self,  mapping_df,batch_size=32, tokenizer = None, max_length = None):
+    def __init__(self,  batch_size=BATCH_SIZE, tokenizer = tokenizer_, max_length = MAX_LENGTH):
         self.tokenizer = tokenizer
         self.max_length = max_length
         self.batch_size = batch_size
-        self.datatype = None
-        self.mapping_df =mapping_df
-        self.tokenizer_type = None
-
     def prepare_train_val_loader(self, train_data, val_data):
-        #(self, dataframe, datatype,loaded_label_encoder, tokenizer_type =None, max_length = None, tokenizer = None):
-        self.datatype = 'train'
-        train_dataset = CustomDataset(train_data,self.datatype,self.mapping_df)
+        train_dataset = CustomDataset(train_data,self.tokenizer)
         train_loader = DataLoader(train_dataset, batch_size=self.batch_size, shuffle=True)
-        val_dataset = CustomDataset(val_data,self.datatype,self.mapping_df )
+        val_dataset = CustomDataset(val_data,self.tokenizer)
         val_loader = DataLoader(val_dataset, batch_size=self.batch_size, shuffle=False)
         return train_loader, val_loader
+    def prepare_test_dev_loader(self, test_data, dev_data):
+        test_dataset = CustomDataset(test_data, self.tokenizer, self.max_length)
+        test_loader = DataLoader(test_dataset, batch_size=self.batch_size, shuffle=False)
+        dev_dataset = CustomDataset(dev_data, self.tokenizer, self.max_length)
+        dev_loader = DataLoader(dev_dataset, batch_size=self.batch_size, shuffle=False)
+        return test_loader,dev_loader
 
-    def prepare_test_loader(self, test_data, mapping_df):
-        test_dataset = CustomDataset(test_data, self.datatype, self.tokenizer, self.max_length)
-        test_loader = DataLoader(test_dataset,mapping_df, batch_size=self.batch_size, shuffle=False)
-        return test_loader
 
-
-
-with open(TRAIN_DATA_LOADER, 'rb') as f:
-    train_loader = pickle.load(f)
-with open(VAL_DATA_LOADER, 'rb') as f:
-    val_loader = pickle.load(f)
-for batch_X,batch_y in train_loader:
-    print(batch_X)
+final_df = pd.read_csv(FINAL_TRAIN_FILE)
+train_df = final_df[final_df['split'] == 'train']
+val_df = final_df[final_df['split'] == 'val']
+data_loader = CustomDataLoader()
+train_loader, val_loader = data_loader.prepare_train_val_loader(train_df, val_df)
+for batch in train_loader:
+    input_id, attention_mask, label =batch
+    print(input_id[0])
     break
+
+
+
+
+
+final_df = pd.read_csv(FINAL_TRAIN_FILE)
+train_df = final_df[final_df['split'] == 'train']
+val_df = final_df[final_df['split'] == 'val']
+
+
+
