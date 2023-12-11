3b87d5d9c7eeaeeb48ddd1d475c1e8563946fe5f
Udbhavkush <ukush4@gwmail.gwu.edu>
2023-12-11 14:49:00 -0500

    few changes in ppt

---
+++ Final_Group_Presentation/pages/11_Conclusion.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title('Conclusion')
+
+st.markdown('''
+    - Tweets on Twitter vary widely due to the absence of proctoring.
+    - BERT's contextual understanding and BM25's ranking gave better results together.
+''')
