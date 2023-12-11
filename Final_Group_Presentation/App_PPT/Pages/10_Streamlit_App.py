a172e9a51f810bc0b63e572b58c69392388f9bea
sunishaharish <sunisha.harish@gmail.com>
2023-12-11 14:46:35 -0500

    Edited ppt slides

--- Final_Group_Presentation/App_PPT/Pages/10_Streamlit_App.py
+++ Final_Group_Presentation/App_PPT/Pages/10_Streamlit_App.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title('Streamlit Application')
 
 st.markdown('''
-    - We've developed an application where users can input a tweet, and the system categorizes it into either a disorder group or a control group.
+    - We've developed an application where users can input a tweet in English or Spanish, and the system categorizes it into either a disorder group or a control group.
     - If the tweet is identified as pertaining to a specific disorder, a chatbot becomes available to offer essential mental health support.
     - Here's the demo of it: 
 ''')
